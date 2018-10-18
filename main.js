b07f1489b306964adb6bb8a9d6c4f8d421afdaf8
xLeRaz <40789305+xLeRaz@users.noreply.github.com>
2018-10-18 23:56:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,13 @@ const hero = new Discord.Client();
 hero.login(process.env.token);
 hero.on('ready',async () => {
   console.log(`Copyright (c) 2018 Copyright iAmYouseFx All Rights Reserved.`);
+  console.log('');
   let spamCounter = 0;
-  let spamLoop = (times, message) => {
-    for(let x = 0; x < times; x++) {
-      let channel = hero.channels.get("502593328219684917");
-      channel.send(message);
-    }
-  }
     setInterval(() => {
       spamCounter++;
-      spamLoop(9, `**، Fun ~ \`${spamCounter}\`**`);
+      for(let x = 0; x < 9; x++) {
+        let channel = hero.channels.get("502593328219684917");
+        channel.send(`**، Fun ~ \`${spamCounter}\`**`);
+      }
     }, 305);
 });
